2d025d5c22d4093e075498fb06e2a08c47b485ee
mx <mx.can.fly.me@gmail.com>
2013-01-21 14:04:25 +0800

    init

--- src/com/lcssit/icms/socket/pack/IDataPackage.java
+++
@@ -1,7 +0,0 @@
-package com.lcssit.icms.socket.pack;
-
-import com.lcssit.icms.socket.pack.i.IPackage;
-
-public abstract class IDataPackage extends IPackage {
-    public abstract void full(String bodyStr);
-}
